44e4c88435a7f412fbf1d028e6f95edb3c6eab93
Priyam Kabra <priyamkabra7@gmail.com>
2026-09-22 17:50:16 -0400

    update from Fall 2026 to Spring 2027

--- site/src/components/course-planner/course-search/CourseSearch.svelte
+++ site/src/components/course-planner/course-search/CourseSearch.svelte
@@ -150,7 +150,7 @@ https://github.com/atcupps/Jupiterp/LICENSE).
   <!-- Course search input and filters [height of 7.5rem] -->
   <div id="planner-course-search" class="px-1 pt-1">
     <div class="ml-1 flex flex-row pb-1 text-xs 2xl:text-sm">
-      <div>Fall 2026</div>
+      <div>Spring 2027</div>
       <div class="grow text-right">Credits: {totalCredits}</div>
     </div>
     <ScheduleSelector />
